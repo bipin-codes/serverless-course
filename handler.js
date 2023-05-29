89d39ebf836a20251adc0b78c3aeb2e92883195a
Bipin Gosain <bipincodes@outlook.com>
2023-05-29 10:32:45 +0530

    some minor changes

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 const DynamoDB = require('aws-sdk/clients/dynamodb');
-const documentClient = new DynamoDB.DocumentClient({ region: 'us-east-1' });
+const documentClient = new DynamoDB.DocumentClient({ region: 'ap-south-1' });
 const NOTES_TABLE_NAME = process.env.NOTES_TABLE_NAME;
 
 const send = (statusCode, data) => {
